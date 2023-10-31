0fc8afc239d53a0fdc4bdc00579d192ed6d869dc
rambaldeo <77577818+rambaldeo@users.noreply.github.com>
2023-10-31 18:53:18 -0400

    Created a sign in page and now working on the home page after the user has logged in

--- app/src/main/java/com/example/capstonefrontend/MainActivity.java
+++ app/src/main/java/com/example/capstonefrontend/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.capstonefrontend;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextInputEditText username, password;
+    Button signIn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        username = findViewById(R.id.usernameInput);
+        password = findViewById(R.id.passwordInput);
+        signIn = findViewById(R.id.signInButton);
+
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent homePage = new Intent(MainActivity.this, HomePage.class);
+                startActivity(homePage);
+            }
+        });
+
     }
 }
