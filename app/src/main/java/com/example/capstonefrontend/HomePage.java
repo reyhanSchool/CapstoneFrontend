0903b497b47cb4b6492c6e41a0d31fed514a44d9
rambaldeo <77577818+rambaldeo@users.noreply.github.com>
2023-10-31 20:00:26 -0400

    Adding files Worked on Home Page

--- app/src/main/java/com/example/capstonefrontend/HomePage.java
+++ app/src/main/java/com/example/capstonefrontend/HomePage.java
@@ -5,10 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class HomePage extends AppCompatActivity {
-
+ /*
+ * The home page should display all of the elders
+ * it should also display the temperature, humidity and also the condition of the room
+ *  */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
+
+        //Going to use a recycler view to display all of the elders
+
     }
 }
